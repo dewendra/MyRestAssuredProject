cbcd37b573ad332360a23528a7ed17057b476c55
Dewendra Singh <dewendra1.singh@gmail.com>
2025-07-30 12:40:50 +0530

    Some changes

--- PetStoreAutomation/src/test/java/api/endpoints/UserEndPoints.java
+++ PetStoreAutomation/src/test/java/api/endpoints/UserEndPoints.java
@@ -13,49 +13,39 @@ import io.restassured.response.Response;
 public class UserEndPoints {
 
 	public static Response createUser(User payload) {
-		Response res=given()
-			.contentType(ContentType.JSON)
-			.accept(ContentType.JSON)
-			.body(payload)
-		
-		.when()
-			.post(Routes.user_post_url);
-		
+		Response res = given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON).body(payload)
+
+				.when().post(Routes.user_post_url);
+
 		return res;
 	}
-	
+
 	public static Response readUser(String userName) {
-		Response res=given()
-			//.contentType(ContentType.JSON)
-			.accept(ContentType.JSON)
-			.pathParam("username", userName)
-		
-		.when()
-			.get(Routes.user_get_url);
-		
+		Response res = given()
+				// .contentType(ContentType.JSON)
+				.accept(ContentType.JSON).pathParam("username", userName)
+
+				.when().get(Routes.user_get_url);
+
 		return res;
 	}
+
 	public static Response updateUser(String userName, User payload) {
-		Response res=given()
-			.contentType(ContentType.JSON)
-			.accept(ContentType.JSON)
-			.pathParam("username", userName)
-			.body(payload)
-		
-		.when()
-			.put(Routes.user_update_url);
-		
+		Response res = given().contentType(ContentType.JSON).accept(ContentType.JSON).pathParam("username", userName)
+				.body(payload)
+
+				.when().put(Routes.user_update_url);
+
 		return res;
 	}
+
 	public static Response deleteUser(String userNname) {
-		Response res=given()
-			.contentType(ContentType.JSON)
-			.accept(ContentType.JSON)
-			.pathParam("username", userNname)
-		
-		.when()
-			.delete(Routes.user_delete_url);
-		
+		Response res = given().contentType(ContentType.JSON).accept(ContentType.JSON).pathParam("username", userNname)
+
+				.when().delete(Routes.user_delete_url);
+
 		return res;
 	}
 }
